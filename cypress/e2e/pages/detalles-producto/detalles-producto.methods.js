ae11a0baeaf130ae3fb18e4e5d7dec4da0a8d06e
Carolina Escobar <carolinaerios@gmail.com>
2023-07-14 03:16:49 +0200

    Agregar producto al carrito

--- cypress/e2e/pages/detalles-producto/detalles-producto.methods.js
+++ cypress/e2e/pages/detalles-producto/detalles-producto.methods.js
@@ -1,11 +1,23 @@
+import { HomeMethods } from "../home/home.methods";
 import { DetallesProductoElements } from "./detalles-producto.elements";
 
 export class DetallesProductoMethods{
     static clickAñadirCesta(){
-        DetallesProductoElements.buttons.añadirCesta.click()
+        DetallesProductoElements.buttons.añadirCesta.click({force: true})
+    }
+    static clickSumarProducto(){
+        DetallesProductoElements.buttons.sumarProducto.click({force: true})
     }
 
     static clickVolverProductos(){
         DetallesProductoElements.buttons.volverProductos.click()
+    }
+
+    static verificacionPaginaDetalleProducto(){
+        DetallesProductoElements.buttons.añadirCesta.should('be.visible')
+    }
+
+    static verificacionProductoAñadido(){
+        HomeMethods.verificarAlerta('Resumen de pedido')
     }
 }
--- cypress/e2e/tests/catalogo-y-compras.cy.js
+++ cypress/e2e/tests/catalogo-y-compras.cy.js
@@ -1,3 +1,4 @@
+import { DetallesProductoMethods } from "../pages/detalles-producto/detalles-producto.methods";
 import { HomeData } from "../pages/home/home.data";
 import { HomeMethods } from "../pages/home/home.methods";
 import { IniciarSesionData } from "../pages/iniciar-sesion/iniciar-sesion.data";
@@ -5,6 +6,7 @@ import { IniciarSesionMethods } from "../pages/iniciar-sesion/iniciar-sesion.met
 import { Logger } from "../util/logger";
 
 const usuario = IniciarSesionData.usuarioValido;
+const producto = 'Base para poste de seguridad'
 
 describe(HomeData.testSuites.catalogoYCompras, () => {
     it('Navegación por categorías', () => {
@@ -27,6 +29,44 @@ describe(HomeData.testSuites.catalogoYCompras, () => {
         cy.wait(10000)
         HomeMethods.verificacionListadoProductos('Base para poste de seguridad')
         HomeMethods.verificacionListadoProductos('Alarm Bar A-Safe')
+        
+    })
+
+    it('Agregar producto al carrito', () => {
+        
+        Logger.stepNumber(1)
+        Logger.step('Iniciar sesión como usuario registrado')
+        Logger.subStep('Navegar a Safeguru')
+        cy.visit("https://safeguru.com/");
+        cy.clearCookies()
+        Logger.subStep('Click en el icono de Cuenta')
+        HomeMethods.clickCuenta();
+        Logger.subStep('Rellenar campos y pulsar botón de inicio de sesión')
+        IniciarSesionMethods.iniciarSesion(usuario.email, usuario.contrasena)
+
+        Logger.stepNumber(2)
+        Logger.step('Seleccionar una categoría de productos en el menú de navegación')
+        HomeMethods.abrirMenu();
+        HomeMethods.abrirSeguridadIndustrial();
+
+        Logger.stepNumber(3)
+        Logger.step('Hacer click en un producto específico')
+        HomeMethods.clickLinkProducto(producto)
+
+        Logger.stepNumber(5)
+        Logger.verificacion('Verificar que se muestra la página de detalles del producto')
+        DetallesProductoMethods.verificacionPaginaDetalleProducto();
+
+        Logger.stepNumber(6)
+        Logger.verificacion('Hacer clic en el botón "Añadir a la cesta"')
+        DetallesProductoMethods.clickAñadirCesta();
+        DetallesProductoMethods.clickSumarProducto();
+        DetallesProductoMethods.clickAñadirCesta();
+
+        Logger.stepNumber(7)
+        Logger.verificacion('Verificar que se muestra un mensaje de confirmación y el producto se agrega al carrito')
+        DetallesProductoMethods.verificacionProductoAñadido();
+       
         
     })
 })
